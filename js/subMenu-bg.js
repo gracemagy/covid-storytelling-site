57cd8e4d6ce06b8f08678cc7cd8a289c4293c434
gracemagy <66710030+gracemagy@users.noreply.github.com>
2020-11-02 18:29:35 +0800

    v1.0

--- js/subMenu-bg.js
+++ js/subMenu-bg.js
@@ -1,21 +1,35 @@
 //change background on subMenu hover
 $("#nav-first-bg").hover(function(){
-     	 	$(this).parent().css("background","url(images/home-bg.jpg) no-repeat center center");
-     	 	$(this).parent().css("background-size","auto 100%");
+     	 	$(this).css("background","url(images/home-bg.jpg) no-repeat center center");
+     	 	$(this).css("background-size","auto 100%");
  		});
 		 $("#nav-second-bg").hover(function(){
-     	 	$(this).parent().css("background","url(images/office-building.jpg) no-repeat center center");
-     	 	$(this).parent().css("background-size","auto 100%");
+     	 	$(this).css("background","url(images/office-building.jpg) no-repeat center center");
+     	 	$(this).css("background-size","auto 100%");
  		});
 		 $("#nav-third-bg").hover(function(){
-     	 	$(this).parent().css("background","url(images/singapore-map.jpg) no-repeat center center");
-     	 	$(this).parent().css("background-size","auto 100%");
+     	 	$(this).css("background","url(images/flying.jpg) no-repeat center center");
+     	 	$(this).css("background-size","auto 100%");
  		});
 
 
-$(".nav-background").mouseover(function() {
-    $(".nav-cta-text").hide().delay(200);
+$(".nav-first a").mouseover(function() {
+    $(".nav-first-img").attr("src", "images/home.svg");
 })
 .mouseout(function() {
-    $(".nav-cta-text").show();
+    $(".nav-first-img").attr("src", "images/home-color.svg");
+});
+
+$(".nav-second a").mouseover(function() {
+    $(".nav-second-img").attr("src", "images/briefcase.svg");
+})
+.mouseout(function() {
+    $(".nav-second-img").attr("src", "images/briefcase-color.svg");
+});
+
+$(".nav-third a").mouseover(function() {
+    $(".nav-third-img").attr("src", "images/airplane.svg");
+})
+.mouseout(function() {
+    $(".nav-third-img").attr("src", "images/airplane-color.svg");
 });
